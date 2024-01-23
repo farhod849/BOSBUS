67022a89edd16d51ec8adb47b14bc3ad449dbbac
Krieger <farhodsabirov42@gmail.com>
2024-01-23 20:12:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bosbus/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.bosbus
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun onClick(v: View?) {
+        val intent = Intent(
+            applicationContext,
+            RaspisanieActivity::class.java
+        )
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/bosbus/RaspisanieActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bosbus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RaspisanieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.raspisanie)
+    }
+
+}
